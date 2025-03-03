a79470628353eededb5885347d5905a504680d13
Marga-Lensen <marga.lensen@dci-student.org>
2025-03-03 23:11:27 +0100

    added vercel.json

--- serverCORS.js
+++ serverCORS.js
@@ -22,6 +22,22 @@ app.use(express.json());
 })); */
 app.use(cors({ origin: 'http://localhost:5173' }));
 
+/* app.use(cors({
+  origin: [
+    "http://localhost:5173",   // For local development
+    // "https://your-frontend.vercel.app"  // Your deployed frontend
+    "https://question-quiz-ten.vercel.app", 
+    //"https://questions-19l495t3g-marga-lensens-projects.vercel.app", 
+    // // ❌ Correct production ❌ frontend URL ❌ NOOO, backend !!!!!!
+
+    // "https://comforting-phoenix-dc4a6c.netlify.app/"
+  ],
+  methods: "GET, POST, PUT, UPDATE, DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+  credentials: true // Allows cookies/auth headers if needed
+})); */
+
+
 // Welcome route at localhost:5000
 app.get("/", (req, res) => {
   res.send("Welcome to the Questions API! The server is up and running.");
@@ -31,6 +47,8 @@ app.get("/", (req, res) => {
 app.use("/", appRouter);
 
 // connectToDb();
+// if alles ok, prima
+// is keine Verbindung, die API läuft immer noch
 
 // Error handling middleware
 app.use(errorMiddleware);
